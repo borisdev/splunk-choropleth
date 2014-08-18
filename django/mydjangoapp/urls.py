3639ff8fe6e7beadb72f0547483637adbb0062c7
boris dev <boris.dev@gmail.com>
2014-08-18 12:57:02 -0700

    chrome detector

--- django/mydjangoapp/urls.py
+++ django/mydjangoapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from splunkdj.utility.views import render_template as render
 
 urlpatterns = patterns('',
-    url(r'^home/$', 'mydjangoapp.views.home', name='home'), 
+    url(r'^home/$', 'sfcrime.views.home', name='home'), 
 )
--- django/mydjangoapp/views.py
+++ django/mydjangoapp/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from splunkdj.decorators.render import render_to
 
-@render_to('mydjangoapp:home.html')
+@render_to('sfcrime:home.html')
 @login_required
 def home(request):
     return {
-        "message": "Hello World from mydjangoapp!",
-        "app_name": "mydjangoapp"
+        "message": "Hello World from San Fran Crime Map!",
+        "app_name": "sfcrime"
     }
